e744cd1b0b75022607cb0e5382b096683ee580f1
Aatmsaat <vaibhav61199@gmail.com>
2019-10-11 23:18:46 +0530

    some changes to view the pattern at every step

--- sakat.py
+++ sakat.py
@@ -1,12 +1,3 @@
-#practice mode example using brute force for visualizing patterns
-#function for counting odd numbers in 2-D
-def oddcount(a,n,m):
-	c=0
-	for i in range(n):
-		for j in range(m):
-			if a[i][j]%2!=0:
-				c+=1
-	return c
 t=int(input())
 for _ in range(t):
 	n,m,q=list(map(int,input().split()))
@@ -15,14 +6,23 @@ for _ in range(t):
 	a=[0]*n
 	for i in range(n):
 		a[i]=[0]*m
-	print(a)
+	count=0
 	while q:
 		q-=1
+		count=0
 		r,c=list(map(int,input().split()))
 		for i in range(m):
 			a[r-1][i]+=1
 		for i in range(n):
 			a[i][c-1]+=1
-		for i in range(n):#printing 2-D array after increasing by with their respective positions
-			print(a[i])
-		print(oddcount(a,n,m))
+		print()
+		for i in range(n):
+			for j in range(m):
+				if a[i][j]%2!=0:
+					count+=1
+					print('1',end=' ')
+				else:
+					print('0',end=' ')
+			print()
+		print(f'{count} \n')
+	#print(count)
